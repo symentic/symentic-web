1b9a6de22e230be473693a697a155498cfe49d60
leog25 <leogao2006@gmail.com>
2025-07-30 21:05:05 -0400

    1

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -125,7 +125,7 @@ const Dashboard: React.FC = () => {
       
       <div className={styles.main}>
         <div className={styles.header}>
-          <h1 className={styles.headerTitle}>welcome to the Origin Layer...</h1>
+          <h1 className={styles.headerTitle}></h1>
           <div className={styles.headerActions}>
             <button 
               className={styles.themeToggle}
